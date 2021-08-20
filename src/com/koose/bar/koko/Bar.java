0b2265c9f2cadd677859671fb4eef03db663a5e7
Koose <KooseTheBlogger@gmail.com>
2021-08-20 13:00:13 +0000

    protect not public will throw error but expection or wont run

--- src/com/koose/bar/koko/Bar.java
+++ src/com/koose/bar/koko/Bar.java
@@ -1,7 +1,7 @@
 package com.koose.bar.koko;
 
 public class Bar { //complier error
-    public void static main (String[] args){ //it should have beeen static before void.
+    protected static void main(String[] args){
         System.out.println("Koose Java");
     }
 }
