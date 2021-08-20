ce83893bc137c42516d9574eeae2025675fc9c78
Koose <KooseTheBlogger@gmail.com>
2021-08-20 05:46:42 +0000

    primitive types and non primitive types play

--- src/com/koose/foo/Foo.java
+++ src/com/koose/foo/Foo.java
@@ -1,4 +1,17 @@
 package com.koose.foo;
 
+import java.time.LocalDate;
+
 public class Foo {
+
+    public static void main(String[] args){
+        // non primitive data types AKA reference types
+        String name = new String("Koose");
+        String gf = new String("Adwoa");
+        LocalDate now = LocalDate.now();
+        System.out.println(now.getDayOfWeek());
+        System.out.println(name.toUpperCase());
+        System.out.println(gf.toUpperCase());
+
+    }
 }
