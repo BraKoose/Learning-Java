900bdacd1ba69b3df862e6848bdce3d430ead6fa
Koose <KooseTheBlogger@gmail.com>
2021-08-20 05:29:54 +0000

    variables

--- src/com/koose/Main.java
+++ src/com/koose/Main.java
@@ -1,15 +1,31 @@
 package com.koose;
+import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
-        /*
-        *   This is a multiline comments
-        *
-        *
-        *
-        * */
-        char a = 'K';
-        System.out.println(a);
+        int numbers = 100;
+        String name = "Koose";
+        Date date = new Date();
+        byte TheByte = 12;
+        short jij = 100;
+        long longBullshit = 100000000L;
+        float pi = 3.14F;
+        double pi2= 3.14567;
+        boolean isAdult = false;
+        boolean isChild = true;
+        char singleChar = 'A';
+
+        System.out.println(numbers);
+        System.out.println(name);
+        System.out.println(date);
+        System.out.println(TheByte);
+        System.out.println(jij);
+        System.out.println(longBullshit);
+        System.out.println(pi);
+        System.out.println(pi2);
+        System.out.println(isAdult);
+        System.out.println(isChild);
+        System.out.println(singleChar);
     }
 }
