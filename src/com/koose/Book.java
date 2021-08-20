8cc3061e405309e6a3b18ea9fa26d34586274d72
Koose <KooseTheBlogger@gmail.com>
2021-08-20 12:26:11 +0000

    i am loving java

--- src/com/koose/Book.java
+++ src/com/koose/Book.java
@@ -1,9 +1,17 @@
 package com.koose;
 
 public class Book {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World:" + args[0]);
-
+    public static void main (String[] args){
+        String content ="1234";
+        int n = Integer.parseInt(content);
+        int m = 2 * n;
+        System.out.println(m + 20);
+        String koose="3.14";
+        double d1 = Double.parseDouble(koose);
+        double d2 = d1 % 100;
+        double d3 = d2 - 140;
+        double d5 = d3 * d1;
+        System.out.println(d5);
     }
+
 }
