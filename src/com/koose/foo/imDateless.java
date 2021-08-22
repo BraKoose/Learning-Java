1c6b70ef3b5cb08a8265b12528494310c36d43a7
Koose <KooseTheBlogger@gmail.com>
2021-08-22 07:26:54 +0000

    Debug maths reserved

--- src/com/koose/foo/imDateless.java
+++ src/com/koose/foo/imDateless.java
@@ -1,4 +1,5 @@
 package com.koose.foo;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.time.LocalDate;
 
@@ -7,7 +8,12 @@ public class imDateless {
     public static void main(String [] args){
         Date date = new Date();
         LocalDate locateDate = LocalDate.now();
-
-
+        LocalDateTime localDateTime = LocalDateTime.now();
+        String s = new String();
+        java.sql.Date dateSql = new java.sql.Date(1);
+        System.out.println(date);
+        System.out.println(localDateTime);
+        System.out.println(s);
+        System.out.println(dateSql);
     }
 }
---
+++ src/com/koose/foo/reserved.java
@@ -0,0 +1,17 @@
+package com.koose.foo;
+
+public class reserved {
+
+    public static void  main(String[] agrs){
+        // arthimetic operations
+        int ten = 10;
+        int two = 2;
+        // BODMAS ORDER
+        int addiction = 10 + 2;
+        System.out.println(addiction);
+        System.out.println(2* 20+(2 * 299)%(10 + 20));
+        System.out.println(100 % (16 * 2400));
+        int nain =0;
+        System.out.println(nain);
+    }
+}
