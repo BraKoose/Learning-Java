225cf6ae8dd2b7cfac2fad78ce1357de305d14d4
Koose <KooseTheBlogger@gmail.com>
2021-08-22 05:13:41 +0000

    reference type with non primitive data types

--- src/com/koose/foo/Foo.java
+++ src/com/koose/foo/Foo.java
@@ -5,13 +5,33 @@ import java.time.LocalDate;
 public class Foo {
 
     public static void main(String[] args){
-        // non primitive data types AKA reference types
-        String name = new String("Koose");
-        String gf = new String("Adwoa");
+        int a = 10;
+        int b = a;
+        a = 100;
         LocalDate now = LocalDate.now();
         System.out.println(now.getDayOfWeek());
-        System.out.println(name.toUpperCase());
-        System.out.println(gf.toUpperCase());
+        System.out.println("a = " + a + " - b" + b);
 
+        Person alex = new Person("Koo$e");
+        Person mariam = alex;
+
+        System.out.println("Before changing Alex");
+        System.out.println(alex.name + " " + mariam.name);
+
+        alex.name = "Godfred Addai Amoako";
+
+
+        System.out.println("After  changing Alex");
+        System.out.println(alex.name + " " + mariam.name);
+
+
+    }
+
+    static class Person{
+        String name;
+
+        Person(String name){
+            this.name = name;
+        }
     }
 }
