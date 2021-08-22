a64c3727d0373170f1a5e1bd2e2f21f74d69ea16
Koose <KooseTheBlogger@gmail.com>
2021-08-22 07:37:42 +0000

    Math. package

--- src/com/koose/foo/reserved.java
+++ src/com/koose/foo/reserved.java
@@ -1,11 +1,19 @@
 package com.koose.foo;
 
+import java.sql.SQLOutput;
+
 public class reserved {
 
     public static void  main(String[] agrs){
         // arthimetic operations
         int ten = 10;
         int two = 2;
+        // You Dont need variables in Maths
+        System.out.println(Math.abs(-10));
+        System.out.println(Math.max(2.78, 10.11));
+        System.out.println(Math.pow(10, 10));
+        System.out.println(Math.sqrt(13));
+        System.out.println(Math.PI);
         // BODMAS ORDER
         int addiction = 10 + 2;
         System.out.println(addiction);
