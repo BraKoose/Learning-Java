b0c92087b5d1d1fb5c11b1edfe37624d9e6d5cdc
Koose <KooseTheBlogger@gmail.com>
2021-08-27 06:44:48 +0000

    factorial of 5

--- src/com/koose/foo/reserved.java
+++ src/com/koose/foo/reserved.java
@@ -5,13 +5,19 @@ import java.sql.SQLOutput;
 public class reserved {
 
     public static void  main(String[] agrs){
-    // Ternary Operators
-        int age = 19;
-        String message = age >= 19 && age < 40 ? "Hooray ....... i am an adult":" I am youthful";
-        if (age >= 56){
-            System.out.println("Hooray ....... i am an adult");
-        }else {
-            System.out.println("Koo$e");
+        // switch Statements
+        String gender = "Female";
+
+        switch ( gender){
+            case "Male":
+                break;
+            case "Female":
+                System.out.println(" I love you Bitch!");
+                break;
+            case "I Prefare Not to Say":
+                break;
+            default:
+                System.out.println(" unknow gender");
         }
     }
 }
